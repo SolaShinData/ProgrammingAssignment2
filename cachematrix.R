ac9ba36f8583b0d6080e48a20cdfd3fb15fd3d87
SolashinData <sola.shin.data@google.com>
2025-04-24 19:57:10 +0900

    Completed matrix inverse caching functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL # inverse 저장 공간
+  
+  set <- function(y){
+    x <<- y
+    inv <<- NULL # 새로운 행렬이 들어오면 캐시 초기화
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  
+  list(set=set, get=get, 
+       setInverse=setInverse, 
+       getInverse=getInverse)
 
 }
 
@@ -12,4 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  if (!is.null(inv)){
+    message("getting cached inverse")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  
+  inv
 }
